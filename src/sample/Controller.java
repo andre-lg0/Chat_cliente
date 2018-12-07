2234d4d122240460884980fbf0e8a4a71bccb4f4
André Luiz <andretm4@hotmail.com>
2018-12-07 20:20:23 -0300

    Conecção com o servidor feita- envia e recebe mensagem
    Signed-off-by: André Luiz <andretm4@hotmail.com>

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,19 +2,17 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class Controller {
 
    private  Socket cliente;
    private PrintWriter msg;
+   private Receber recebe;
 
    @FXML
    private TextField Message;
@@ -27,29 +25,45 @@ public class Controller {
 
 
 
-        public Controller() {
-            Rede();
-        }
 
+   public Controller() {
+       Rede();
 
-    private  void Rede(){
+   }
+
+// Metodo para configuração do cliente e receber dados do servidor
+        private  void Rede(){
         try {
-            cliente = new Socket("127.0.0.1", 12345);
+            cliente = new Socket("127.0.0.1", 5000);
               msg =  new PrintWriter(cliente.getOutputStream());
-        }catch (Exception e){
+              // classe recebe trata a mensagem que chega do servidor e envia para o chat
+              recebe = new Receber(cliente.getInputStream(), this);
+              Thread t  =  new Thread(recebe);
+              t.start();
 
+        }catch (Exception e){
+            System.out.println("problema na conexao do servidor" + e);
         }
 
 
 
     }
-
+    // metodo que pegar a entrada do usuario e envia para o servidor
     @FXML
     private void Send(){
             this.msg.println(this.Message.getText());
+            this.msg.flush();
             this.textChat.appendText("Usuario: " + this.Message.getText() + "\n");
             this.Message.setText("");
+
     }
 
+    public void recebido(String msg){
+       textChat.appendText(msg + "\n");
+    }
 
+    @FXML
+    private void close(){
+
+    }
 }
