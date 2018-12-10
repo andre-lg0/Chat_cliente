402157c8159b7bee5f3548f5fb79a12864303bc3
André Luiz <andretm4@hotmail.com>
2018-12-10 14:50:54 -0300

    Socket para envio de arquivo completo
    Signed-off-by: André Luiz <andretm4@hotmail.com>

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,13 +4,21 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.sql.SQLOutput;
 
 public class Controller {
 
    private  Socket cliente;
+   private Socket arquivo;
    private PrintWriter msg;
    private Receber recebe;
 
@@ -23,21 +31,24 @@ public class Controller {
    @FXML
    private Button enviar;
 
-
+   private FileChooser choose;
 
 
    public Controller() {
        Rede();
+       this.choose = new FileChooser();
+
 
    }
 
 // Metodo para configuração do cliente e receber dados do servidor
         private  void Rede(){
         try {
-            cliente = new Socket("127.0.0.1", 5000);
-              msg =  new PrintWriter(cliente.getOutputStream());
+            this.cliente = new Socket("127.0.0.1", 5000);
+            this.arquivo = new Socket("127.0.0.1", 5001);
+             this.msg =  new PrintWriter(cliente.getOutputStream());
               // classe recebe trata a mensagem que chega do servidor e envia para o chat
-              recebe = new Receber(cliente.getInputStream(), this);
+              this.recebe = new Receber(cliente.getInputStream(), this);
               Thread t  =  new Thread(recebe);
               t.start();
 
@@ -53,7 +64,7 @@ public class Controller {
     private void Send(){
             this.msg.println(this.Message.getText());
             this.msg.flush();
-            this.textChat.appendText("Usuario: " + this.Message.getText() + "\n");
+            //this.textChat.appendText("Usuario: " + this.Message.getText() + "\n");
             this.Message.setText("");
 
     }
@@ -63,7 +74,30 @@ public class Controller {
     }
 
     @FXML
-    private void close(){
+    private void Arquivo() {
+       cofiguraFile(this.choose);
+       File file =  this.choose.showOpenDialog(null);
+       if(file != null);
+       try {
+           byte[]buffer = new byte[1024];
+           int bytes;
+           FileInputStream FileEn = new FileInputStream(file);
+           System.out.println("lendo o arquivo");
+           OutputStream envia  = this.arquivo.getOutputStream();
+           // enviando o arquivo
+           while((bytes = FileEn.read(buffer))!= -1 ){
+               envia.write(buffer,0,bytes);
+               envia.flush();
+           }
 
+       }catch (Exception e){
+           System.out.println("erro no arquvo");
+       }
+
+    }
+
+    private static void cofiguraFile(FileChooser file){
+       file.setTitle("Seleciona o arquivo");
+       file.setInitialDirectory(new File(System.getProperty("user.home")));
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,8 +12,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("MSN");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(new Scene(root, 850, 620));
         primaryStage.show();
+
         //primaryStage.getOnCloseRequest();
     }
 
